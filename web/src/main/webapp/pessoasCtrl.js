573e045ef84b95df67bf898e36574bf15829998a
marcos2250 <marcos2250@gmail.com>
2015-08-18 10:16:41 -0300

    ignorando arquivos

--- web/src/main/webapp/pessoasCtrl.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module('myApp', []);
-app.controller('pessoasCtrl', function($scope, $http) {
-    $http.get("http://localhost:8080/pessoas")
-    .success(function (response) {$scope.pessoas = response.pessoas;});
-});
