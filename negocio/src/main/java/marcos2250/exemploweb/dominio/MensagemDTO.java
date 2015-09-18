378e49a3f71d50b9f189a8bede7579b10671c674
Marcos M. Meneses <marcos2250@gmail.com>
2015-09-18 18:58:43 -0300

    error handling

---
+++ negocio/src/main/java/marcos2250/exemploweb/dominio/MensagemDTO.java
@@ -0,0 +1,14 @@
+package marcos2250.dominio;
+
+public class MensagemDTO {
+
+    private String mensagem;
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}
